407ede01801085d188606346799af60a5882d2ab
jatinbky64 <jatin@odinschool.com>
2025-01-21 14:52:04 +0530

    assets size limit updated

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        providerOptions: {
+          sizeLimit: 100000000, // 100 MB (Adjust as needed)
+        },
+      },
+    },
+  });
+  
